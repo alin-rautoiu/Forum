6c46cd2336ab014f6b66ec86239c8553528b991a
alin-rautoiu <alin.rautoiu@cti.pub.ro>
2013-12-13 10:52:57 +0200

    See list of discussions, empty discussion page

---
+++ Forum.Models/Discussion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+    public class Discussion: IEntity
+    {
+        public Guid Id
+        {
+            get;
+            set;
+        }
+
+        public string Title { get; set; }
+    }
+}
---
+++ Forum/Controllers/ThreadController.cs
@@ -0,0 +1,41 @@
+﻿using Forum.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forum.Controllers
+{
+    public class ThreadController : Controller
+    {
+        public ActionResult Index()
+        {
+            var discussions = new List<Discussion>
+            {
+                new Discussion {Id=Guid.NewGuid(), Title="Despre cocosi"},
+                new Discussion {Id=Guid.NewGuid(), Title="Despre compostoare"}
+            };
+            return View(discussions);
+        }
+
+        public ActionResult See(Guid id)
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
